4dc08cad2e49237340ed143bd8b9fe343edeebf7
hemalatha <hemavelusamy9@gmail.com>
2026-06-13 15:18:50 +0530

    ml model integration and dashboard prediction

--- core_app/views.py
+++ core_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render,redirect
+import joblib
+import os
+model = joblib.load('model.pkl')
 def login_view(request):
     if request.method == 'POST':
         return redirect('dashboard')
     return render(request,'core_app/login.html')
 def dashboard_view(request):
-    return render(request,'core_app/dashboard.html')
+    prediction = "Pending..." # Start-la ethuvum illana ithu theriyum
+    if request.method == 'POST':
+        cgpa = float(request.POST.get('cgpa'))
+        attendance = int(request.POST.get('attendance'))
+        
+        # Model prediction
+        result = model.predict([[cgpa, attendance]])
+        prediction = "Placed" if result[0] == 1 else "Not Placed"
+    
+    return render(request, 'core_app/dashboard.html', {'prediction': prediction})
